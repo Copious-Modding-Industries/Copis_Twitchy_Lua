0a63a370e2789327f450ccf45812f6cf53750b23
Human <54731850+Ramiels@users.noreply.github.com>
2022-12-15 17:13:38 -0500

    tried new stuff

--- init.lua
+++ init.lua
@@ -5,34 +5,13 @@ ModLuaFileAppend("data/scripts/streaming_integration/event_utilities.lua", "mods
 Write = ModTextFileSetContent
 Read = ModTextFileGetContent
 
+-- ???
+dofile("data/scripts/streaming_integration/event_utilities.lua")
+
 -- Run every frame
 function OnWorldPreUpdate()
 	-- Check if connected to twitch
-	if StreamingGetIsConnected() then
-		if Write ~= nil then
-			-- Only run when a new script is available
-			if GlobalsGetValue("funny_this_frame", "0") == "1" then
-				-- Get data from twitch lua context
-				local funny = GlobalsGetValue("funny_lua", "")
-				local user = GlobalsGetValue("funny_user", "")
-				local times = GlobalsGetValue("funny_virtual_times", "0")
-				local path = ("mods/Copis_Twitchy_Lua/virtual_files/funny_%s_.lua"):format(times)
-				-- Attempt to execute code
-				local s1 = pcall(Write, path, tostring(funny))
-				local s2 = pcall(dofile, path)
-				-- Check if succeeded
-				if not (s1 and s2) then
-					print(("[CopiTLua]: Executed script %s, by user %s"):format(path, user))
-				else
-					GamePrintImportant(user .. "'s script errored!", ("Write success: %s, dofile success: %s"):format(tostring(s1), tostring(s2)), "data/ui_gfx/decorations/conductive.png")
-					print(("[CopiTLua]: Errored script %s, by user %s"):format(path, user))
-				end
-				-- End execution
-				GlobalsSetValue("funny_this_frame", "0")
-				GlobalsSetValue("funny_virtual_times", tostring(tonumber(times) + 1))
-			end
-		end
-	else
+	if not StreamingGetIsConnected() then
 		-- Warn user if twitch is disconnected
 		Gui = Gui or GuiCreate()
 		GuiStartFrame(Gui)
--- the_funny.lua
+++ the_funny.lua
@@ -1,15 +1,25 @@
-local old_streaming_on_irc = _streaming_on_irc
+-- Counter for times run
+Twitchy_Lua_Count = 0
 -- monkey patch message recieved callback
+local old_streaming_on_irc = _streaming_on_irc
 function _streaming_on_irc( is_userstate, sender_username, message, raw )
 	-- check if user has permissions to run Lua
 	if (sender_username:lower() == ModSettingGet("Copis_Twitchy_Lua.poweruser"):lower() or "") or ModSettingGet("Copis_Twitchy_Lua.pandemonium") then
 		-- Check if message is a Lua script
 		if message:match("LUA: ") then
-			-- Feed data to init.lua
 			message = message:gsub("LUA: ", "")
-			GlobalsSetValue( "funny_this_frame", "1")
-			GlobalsSetValue( "funny_lua", message)
-			GlobalsSetValue( "funny_user", sender_username)
+			local path = ("mods/Copis_Twitchy_Lua-main/virtual_files/funny_%s_.lua"):format(Twitchy_Lua_Count)
+			-- Attempt to execute code
+			local s1 = pcall(ModTextFileSetContent, path, tostring(message))
+			local s2 = pcall(dofile, path)
+			-- Check if succeeded
+			if s1 and s2 then
+				print(("[CopiTLua]: Executed script %s, by user %s"):format(path, sender_username))
+			else
+				GamePrintImportant(sender_username .. "'s script errored!", ("Write success: %s, dofile success: %s"):format(tostring(s1), tostring(s2)), "data/ui_gfx/decorations_biome_modifier/conductive.png")
+				print(("[CopiTLua]: Errored script %s, by user %s"):format(path, sender_username))
+			end
+			Twitchy_Lua_Count = Twitchy_Lua_Count + 1
 		end
 	end
 	-- run original code
