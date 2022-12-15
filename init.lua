c2f626dbb60e62ef7454473756b4b6a56506c566
Human <54731850+Ramiels@users.noreply.github.com>
2022-12-15 16:49:22 -0500

    Gui

--- init.lua
+++ init.lua
@@ -36,13 +36,23 @@ function OnWorldPreUpdate()
 		-- Warn user if twitch is disconnected
 		Gui = Gui or GuiCreate()
 		GuiStartFrame(Gui)
-		local msg = "Twitch Disconnected!"
-		local sw, sh = GuiGetScreenDimensions(Gui)
-		local tw, th = GuiGetTextDimensions(Gui, msg)
-		local gx, gy = (sw - tw) / 2, (sh - th) / 2
-		GuiOptionsAddForNextWidget(Gui, 25)
-		GuiColorSetForNextWidget(Gui, 0.8, 0.6, 0, 1)
-		GuiText(Gui, gx, gy, msg)
+		GuiIdPushString(Gui, "Copis_Twitchy_Lua")
+			-- Popup
+			GuiBeginAutoBox(Gui)
+				local msg = "Twitch Disconnected!"
+				local sw, sh = GuiGetScreenDimensions(Gui)
+				local tw, th = GuiGetTextDimensions(Gui, msg)
+				local iw, ih = GuiGetImageDimensions(Gui, "data/ui_gfx/inventory/icon_warning.png", 2)
+				local tx, ty = (sw - tw) / 2, ((sh / 2) - th) / 2
+				local ix, iy = (sw - iw) / 2, ((sh / 2) - ih) / 2
+				GuiOptionsAddForNextWidget(Gui, 25)
+				GuiColorSetForNextWidget(Gui, 0.8, 0.6, 0, 1)
+				GuiText(Gui, tx, ty + 10, msg)
+				GuiOptionsAddForNextWidget(Gui, 25)
+				GuiImage(Gui, 1, ix, iy - 15, "data/ui_gfx/inventory/icon_warning.png", 1, 2, 2)
+			GuiZSetForNextWidget(Gui, 5)
+			GuiEndAutoBoxNinePiece(Gui, 5, 0, 0, false, 0, "data/ui_gfx/decorations/9piece0.png", "data/ui_gfx/decorations/9piece0.png")
+		GuiIdPop(Gui)
 	end
 end
--- TODO: filter mod setting and persistent flag setting code, look for any vulnerabilities in code,
+-- TODO: filter mod setting and persistent flag setting code, look for any vulnerabilities in code, message queue
