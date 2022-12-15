4e94979002b82b2b0ec9010b7b2818028cad89a1
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-12-16 00:28:19 +0100

    Proof of concept tamper protection

--- the_funny.lua
+++ the_funny.lua
@@ -1,8 +1,13 @@
+-- Use local functions/variables so they can't be changed by the chatter
+
 -- Counter for times run
-Twitchy_Lua_Count = 0
+local Twitchy_Lua_Count = 0
+
+local install_hooks = nil
+
 -- monkey patch message recieved callback
 local old_streaming_on_irc = _streaming_on_irc
-function _streaming_on_irc( is_userstate, sender_username, message, raw )
+function streaming_on_irc_hook( is_userstate, sender_username, message, raw )
 	-- check if user has permissions to run Lua
 	if (sender_username:lower() == ModSettingGet("Copis_Twitchy_Lua.poweruser"):lower() or "") or ModSettingGet("Copis_Twitchy_Lua.pandemonium") then
 		-- Check if message is a Lua script
@@ -14,6 +19,8 @@ function _streaming_on_irc( is_userstate, sender_username, message, raw )
 			local func = loadfile(path)
 			if func ~= nil then
 				pcall(func)
+				-- Immediately reinstall hooks to make sure chatter can't override something important
+				install_hooks()
 				print(("[CopiTLua]: Executed script %s, by user %s"):format(path, sender_username))
 			else
 				GamePrintImportant(sender_username .. "'s script errored!", path, "data/ui_gfx/decorations_biome_modifier/conductive.png")
@@ -27,3 +34,27 @@ function _streaming_on_irc( is_userstate, sender_username, message, raw )
 		old_streaming_on_irc( is_userstate, sender_username, message, raw )
 	end
 end
+
+local original_EntityKill = EntityKill
+local function guarded_EntityKill(entity_id)
+	-- Can't circumvent checks by using a string for the entity id
+	entity_id = tonumber(entity_id)
+
+	local player_entities = EntityGetWithTag("player_unit")
+	for _, player_entity in ipairs(player_entities) do
+		if entity_id == player_entity then
+			print("[CopiTLua]: Prevented player from getting killed")
+			return
+		end
+	end
+
+	return original_EntityKill(entity_id)
+end
+
+install_hooks = function()
+	_streaming_on_irc = streaming_on_irc_hook
+	EntityKill = guarded_EntityKill
+end
+
+-- Initial install
+install_hooks()
