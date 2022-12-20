1c97cf90dc14fada09887294fff7dd94dd8e92dd
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-12-20 18:35:45 +0100

    Remove code that does nothing

--- init.lua
+++ init.lua
@@ -1,10 +1,4 @@
 -- Inject the_funny.lua
 ModLuaFileAppend("data/scripts/streaming_integration/event_utilities.lua", "mods/Copis_Twitchy_Lua/the_funny.lua")
 
--- Save ModTextFile functions past initialization
-Write = ModTextFileSetContent
-Read = ModTextFileGetContent
-
--- Somehow, this works.
-dofile("data/scripts/streaming_integration/event_utilities.lua")
 -- TODO: filter mod setting and persistent flag setting code, look for any vulnerabilities in code
