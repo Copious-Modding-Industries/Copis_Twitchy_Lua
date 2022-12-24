de74a2edcf8c4215293218ed72a2cd1baa2300d3
Human <54731850+Ramiels@users.noreply.github.com>
2022-12-23 22:23:42 -0500

    Update the_funny.lua

--- the_funny.lua
+++ the_funny.lua
@@ -53,9 +53,55 @@ local function guarded_EntityKill(entity_id)
 	return original_EntityKill(entity_id)
 end
 
+-- Prevent mod setting tampering
+local original_ModSettingSet = ModSettingSet
+local function guarded_ModSettingSet(id, value)
+	if not ModSettingGet("Copis_Twitchy_Lua.chaos") then
+		return original_ModSettingSet(id, value)
+	else
+		print("[CopiTLua]: Prevented ModSettingSet from being called")
+		return
+	end
+end
+
+local original_ModSettingSetNextValue = ModSettingSetNextValue
+local function guarded_ModSettingSetNextValue(id, value, is_default)
+	if not ModSettingGet("Copis_Twitchy_Lua.chaos") then
+		return original_ModSettingSetNextValue(id, value, is_default)
+	else
+		print("[CopiTLua]: Prevented ModSettingSetNextValue from being called")
+		return
+	end
+end
+
+-- Prevent persistent flag tampering
+local original_AddFlagPersistent = AddFlagPersistent
+local function guarded_AddFlagPersistent(key)
+	if not ModSettingGet("Copis_Twitchy_Lua.chaos") then
+		return original_AddFlagPersistent(key)
+	else
+		print("[CopiTLua]: Prevented AddFlagPersistent from being called")
+		return
+	end
+end
+
+local original_RemoveFlagPersistent = RemoveFlagPersistent
+local function guarded_RemoveFlagPersistent(key)
+	if not ModSettingGet("Copis_Twitchy_Lua.chaos") then
+		return original_RemoveFlagPersistent(key)
+	else
+		print("[CopiTLua]: Prevented RemoveFlagPersistent from being called")
+		return
+	end
+end
+
 install_hooks = function()
 	_streaming_on_irc = streaming_on_irc_hook
 	EntityKill = guarded_EntityKill
+	ModSettingSet = guarded_ModSettingSet
+	ModSettingSetNextValue = guarded_ModSettingSetNextValue
+	AddFlagPersistent = guarded_AddFlagPersistent
+	RemoveFlagPersistent = guarded_RemoveFlagPersistent
 end
 
 -- Initial install
