a26c77b41ec9ee6cbcb7e3b81f078427f60547bd
Human <54731850+Ramiels@users.noreply.github.com>
2023-02-18 11:14:16 -0500

    Upload preparations

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
--- Inject the_funny.lua
-ModLuaFileAppend("data/scripts/streaming_integration/event_utilities.lua", "mods/Copis_Twitchy_Lua/the_funny.lua")
+-- Inject event_utilities_append.lua
+ModLuaFileAppend("data/scripts/streaming_integration/event_utilities.lua", "mods/Copis_Twitchy_Lua/event_utilities_append.lua")
