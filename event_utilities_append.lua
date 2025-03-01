70f04be79b9cf2a007abfaf55df60ef26f787dd4
Copi <54731850+Ramiels@users.noreply.github.com>
2025-03-01 01:52:36 -0500

    multiuser support + pre-set variables

--- event_utilities_append.lua
+++ event_utilities_append.lua
@@ -7,26 +7,48 @@ local old_streaming_on_irc = _streaming_on_irc
 
 function streaming_on_irc_hook( is_userstate, sender_username, message, raw )
 	-- check if user has permissions to run Lua
-	if (sender_username:lower() == ModSettingGet("Copis_Twitchy_Lua.poweruser"):lower() or "") or ModSettingGet("Copis_Twitchy_Lua.pandemonium") then
+	local valid = false
+	---@diagnostic disable-next-line: param-type-mismatch
+	for i in string.gmatch(ModSettingGet("Copis_Twitchy_Lua.poweruser"):lower(), "%S+") do
+		if sender_username:lower() == i or "" then
+			valid = true
+			break
+		end
+	end
+	-- if so, do stuff
+	if (valid) or ModSettingGet("Copis_Twitchy_Lua.pandemonium") then
 		-- Check if message is a Lua script
 		if message:match("LUA: ") then
-
 			message = message:gsub("LUA: ", "")
 			local path = "mods/Copis_Twitchy_Lua-main/virtual_files/funny.lua"
 			-- Attempt to execute code
 			ModTextFileSetContent(path, message)
 			local func = loadfile(path)
 			if func ~= nil then
+				-- Setup to save chars
+				local Player = EntityGetWithTag("player_unit")[1]
+				local Px, Py, Pr, Psx, Psy = EntityGetTransform(Player)
+				local Mx, My = DEBUG_GetMouseWorld()
+				local Cx, Cy = GameGetCameraPos()
+				local Comp = EntityGetFirstComponentIncludingDisabled
+				local function ploop(fn)
+					local ps = EntityGetWithTag("player_unit")
+					for i=1, #ps do
+						fn(ps[i])
+					end
+				end
+
 				pcall(func)
 				-- Immediately reinstall hooks to make sure chatter can't override something important
 				install_hooks()
 				print(("[CopiTLua]: Executed script %s, by user %s"):format(path, sender_username))
-
 			else
 				GamePrintImportant(sender_username .. "'s script errored!", path, "data/ui_gfx/decorations_biome_modifier/conductive.png")
 				print(("[CopiTLua]: Errored script %s, by user %s"):format(path, sender_username))
-
 			end
+		elseif message:match("GPI: ") then
+			message = message:gsub("GPI: ", "")
+			GamePrintImportant(message, sender_username, "mods/Copis_Twitchy_Lua/3piece_meta.png")
 		end
 	end
 
@@ -46,8 +68,8 @@ local function guarded_EntityKill(entity_id)
 	entity_id = tonumber(entity_id)
 
 	local player_entities = EntityGetWithTag("player_unit")
-	for _, player_entity in ipairs(player_entities) do
-		if entity_id == player_entity then
+	for i = 1, #player_entities  do
+		if entity_id == player_entities[i] then
 			print("[CopiTLua]: Prevented player from getting killed")
 			return
 		end
